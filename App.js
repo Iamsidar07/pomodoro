c4f152cf378c05e19c1e8cff73881e4bd8ed88bc
Manoj Kumar <ms8460149@gmail.com>
2024-01-30 08:10:08 +0530

    setup starting project

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeWindStyleSheet } from 'nativewind';
+
+NativeWindStyleSheet.setOutput({
+  default: "native"
+})
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 w-full">
+      <Text className="text-green-500 text-4xl">Open up App.js to start working on your app!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
