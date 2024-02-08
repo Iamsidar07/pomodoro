31f0bc21d77b5ce72128777fc46fa599e64ab67b
Manoj Kumar <ms8460149@gmail.com>
2024-02-08 19:59:13 +0530

    setup tailwindcss

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { NativeWindStyleSheet } from 'nativewind';
 
 NativeWindStyleSheet.setOutput({
